ff0e35d430f78afcfdd9792d0f0c035a90c63045
ismaelpaul <ismaafp@gmail.com>
2022-11-19 08:21:05 +0000

    Add async function

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,7 +2,7 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default function handler(req, res) {
+export default async function (req, res) {
 	const body = JSON.parse(req.body);
 
 	const message = `
