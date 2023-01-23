779a3977ade1b2cd4e8ea78e04ee52092532871a
ismaelpaul <ismaafp@gmail.com>
2023-01-23 14:32:11 +0000

    Reorder icons

--- components/Invent.js
+++ components/Invent.js
@@ -10,61 +10,63 @@ import styles from '../styles/Invent.module.scss';
 const Invent = () => {
 	const { ref: containerRef, inView: isContainerVisible } = useInView();
 
-	<div
-		ref={containerRef}
-		className={`${styles.container} ${
-			isContainerVisible ? styles.containerAnimated : ''
-		}`}
-	>
-		<h3>Invent</h3>
-		<Image
-			className={styles.invent}
-			src={inventMockup}
-			alt="Notebook mockup displaying Invent app interface"
-		/>
-		<p>A Full Stack inventory app.</p>
-		<div className={styles.icons}>
-			<Tooltip content={'React'} css={{ fontFamily: 'OpenSans-Regular' }}>
-				<FaReact />
-			</Tooltip>
+	return (
+		<div
+			ref={containerRef}
+			className={`${styles.container} ${
+				isContainerVisible ? styles.containerAnimated : ''
+			}`}
+		>
+			<h3>Invent</h3>
+			<Image
+				className={styles.invent}
+				src={inventMockup}
+				alt="Notebook mockup displaying Invent app interface"
+			/>
+			<p>A Full Stack inventory app.</p>
+			<div className={styles.icons}>
+				<Tooltip content={'React'} css={{ fontFamily: 'OpenSans-Regular' }}>
+					<FaReact />
+				</Tooltip>
 
-			<Tooltip content={'Node.js'} css={{ fontFamily: 'OpenSans-Regular' }}>
-				<FaNodeJs />
-			</Tooltip>
-			<Tooltip content={'Express'} css={{ fontFamily: 'OpenSans-Regular' }}>
-				<SiExpress />
-			</Tooltip>
-			<Tooltip content={'MongoDB'} css={{ fontFamily: 'OpenSans-Regular' }}>
-				<SiMongodb />
-			</Tooltip>
+				<Tooltip content={'Redux'} css={{ fontFamily: 'OpenSans-Regular' }}>
+					<SiRedux />
+				</Tooltip>
+				<Tooltip content={'SASS'} css={{ fontFamily: 'OpenSans-Regular' }}>
+					<DiSass />
+				</Tooltip>
+				<Tooltip content={'Node.js'} css={{ fontFamily: 'OpenSans-Regular' }}>
+					<FaNodeJs />
+				</Tooltip>
+				<Tooltip content={'Express'} css={{ fontFamily: 'OpenSans-Regular' }}>
+					<SiExpress />
+				</Tooltip>
+				<Tooltip content={'MongoDB'} css={{ fontFamily: 'OpenSans-Regular' }}>
+					<SiMongodb />
+				</Tooltip>
 
-			<Tooltip content={'SASS'} css={{ fontFamily: 'OpenSans-Regular' }}>
-				<DiSass />
-			</Tooltip>
-			<Tooltip content={'Redux'} css={{ fontFamily: 'OpenSans-Regular' }}>
-				<SiRedux />
-			</Tooltip>
-			<Tooltip content={'Netlify'} css={{ fontFamily: 'OpenSans-Regular' }}>
-				<SiNetlify />
-			</Tooltip>
+				<Tooltip content={'Netlify'} css={{ fontFamily: 'OpenSans-Regular' }}>
+					<SiNetlify />
+				</Tooltip>
+			</div>
+			<div className={styles.buttons}>
+				<a
+					href="https://invent-app.netlify.app/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<button>Project</button>
+				</a>
+				<a
+					href="https://github.com/ismaelpaul/fe-invent"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<button>Github</button>
+				</a>
+			</div>
 		</div>
-		<div className={styles.buttons}>
-			<a
-				href="https://invent-app.netlify.app/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<button>Project</button>
-			</a>
-			<a
-				href="https://github.com/ismaelpaul/fe-invent"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<button>Github</button>
-			</a>
-		</div>
-	</div>;
+	);
 };
 
 export default Invent;
